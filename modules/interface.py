f5958e73e645913fa4fd79aa3af8f4c0f80b0392
Nicolas Yang <nicolas.yang.1@gmail.com>
2022-05-06 14:36:47 -0300

    add data frame treatment

--- modules/interface.py
+++ modules/interface.py
@@ -1,66 +1,72 @@
 from tkinter import *
-from modules.files import import_file
-from modules.models import cluster, linear_regression
+from tkinter import filedialog
+
+from models import cluster, linear_regression
 
 
 def display_score(model, x_test, y_test, model_test):
-    # s = round(model.score(x_test, y_test), 5)
-    # MAE = round(mean_absolute_error(y_test, model_test), 5)
-    # MBE = round(((y_test - model_test).sum()) / model_test.size, 5)
-    # RMSE = round(np.sqrt(mean_squared_error(y_test, model_test)), 5)
-    # R2 = round(r2_score(y_test, model_test), 5)
-    # maxE = round(max_error(y_test, model_test), 5)
-    # score = tk.Label(text="score : " + s.astype(str) + "\n MAE : " +
-    #                  MAE.astype(str) + "\n MBE : " + MBE.astype(str) +
-    #                  "\n RMSE : " + RMSE.astype(str) + "\n R2 : " +
-    #                  R2.astype(str) + "\n maxE : " + maxE.astype(str))
-    # score.grid(row=2, column=0)
-
-    # fig = plt.figure(figsize=(3, 2))
-    # plt.scatter(y_test, model_test, c='r', label='Predict')
-    # plt.plot(y_test, y_test, label='Real')
-    # plt.legend()
-    # plt.title('Test')
-    # plt.xlabel('GHI_Target real')
-    # plt.ylabel('GHI_Target predict')
     return False
 
 
-def display_window():
+def import_file(dialog_title="importer un fichier"):
     """
-    It creates a window with a button that, when clicked, calls the function import_file() which opens a
-    file dialog and returns the path of the file selected by the user
+    It opens a file dialog, and returns the file object.
+    
+    :param dialog_title: The title of the dialog box, defaults to importer un fichier (optional)
+    :return: The file object.
     """
-    window = Tk()
+    global imported_file
 
-    def execute():
-        if (var.get() == 1):
-            linear_regression()
-        elif (var.get() == 2):
-            cluster()
-        else:
-            print("error")
+    file_location = filedialog.askopenfilename(
+        initialdir=r"./datas_test",
+        title=dialog_title,
+        filetypes=(("text files", "*.csv"), ("all files", "*.*")))
+    imported_file = open(file_location, 'r')
+    return imported_file
+    # showinfo("INFO", "Fichier importé")
 
-    import_label = Label(text="Cliquer pour importer un dataframe")
-    import_btn = Button(text="import", command=import_file)
 
-    var = IntVar()
-    var.set(1)
-    linear_regression_radio_choice = Radiobutton(window,
-                                                 text="Régression linéaire",
-                                                 variable=var,
-                                                 value=1)
-    cluster_radio_choice = Radiobutton(window,
-                                       text="Clustering",
-                                       variable=var,
-                                       value=2)
+def import_4_files(dialog_title="importer un fichier"):
+    """
+    It imports 4 files, and returns them as a tuple
+    
+    :param dialog_title: The title of the dialog box, defaults to importer un fichier (optional)
+    :return: A tuple of 4 files.
+    """
+    file_1 = import_file(dialog_title + "1")
+    file_2 = import_file(dialog_title + "2")
+    file_3 = import_file(dialog_title + "3")
+    file_4 = import_file(dialog_title + "4")
+    return file_1, file_2, file_3, file_4
 
-    execute_btn = Button(text="executer", command=execute)
 
-    import_label.pack()
-    import_btn.pack()
-    linear_regression_radio_choice.pack()
-    cluster_radio_choice.pack()
-    execute_btn.pack()
+def execute():
+    if (var.get() == 1):
+        linear_regression(imported_file)
+    elif (var.get() == 2):
+        cluster()
+    else:
+        print("error")
 
-    window.mainloop()
+
+window = Tk()
+import_label = Label(text="Cliquer pour importer un dataframe")
+import_btn = Button(text="import", command=import_file)
+var = IntVar()
+var.set(1)
+linear_regression_radio_choice = Radiobutton(window,
+                                             text="Régression linéaire",
+                                             variable=var,
+                                             value=1)
+cluster_radio_choice = Radiobutton(window,
+                                   text="Clustering",
+                                   variable=var,
+                                   value=2)
+
+execute_btn = Button(text="executer", command=execute)
+import_label.pack()
+import_btn.pack()
+linear_regression_radio_choice.pack()
+cluster_radio_choice.pack()
+execute_btn.pack()
+window.mainloop()
--- modules/models.py
+++ modules/models.py
@@ -1,6 +1,46 @@
-def linear_regression():
-    print("linear regression")
-    return "Régression linéaire"
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas
+from sklearn.linear_model import LinearRegression
+
+
+def data_frame_treatment(data_frame):
+    """
+    It takes a dataframe, and returns a new dataframe with the same columns, but with the rows that are
+    not consecutive removed
+    
+    :param data_frame: the dataframe that you want to treat
+    :return: A dataframe with the same columns as the input dataframe, but with the last column being
+    the GHI(t+1) column.
+    """
+    data_frame = pandas.read_csv(data_frame, sep=';')
+    data_frame_new = data_frame.copy()
+
+    data_frame_new["GHI(t+1)"] = np.nan
+    data_frame_new.loc[:len(data_frame_new) - 2, "GHI(t+1)"] = np.array(
+        data_frame_new.loc[1:len(data_frame) - 1, "Date"]) - np.array(
+            data_frame_new.loc[:len(data_frame_new) - 2, "Date"])
+    data_frame_new[(data_frame_new["GHI(t+1)"] != 1)] = np.nan
+    index_with_nan = data_frame_new.index[data_frame_new.isnull().any(axis=1)]
+    data_frame_new.drop(index_with_nan, 0, inplace=True)
+    data_frame_new = data_frame_new.append(data_frame.loc[len(data_frame) - 1],
+                                           ignore_index=True)
+
+    data_frame_new.loc[:len(data_frame_new) - 2, "GHI(t+1)"] = np.array(
+        data_frame_new.loc[1:len(data_frame) - 1, "GHI(Wh/m2)"])
+    return data_frame_new
+
+
+def linear_regression(file):
+    data_frame = data_frame_treatment(file)
+    print(data_frame)
+    plt.plot(data_frame["Date"], data_frame["GHI(t+1)"])
+    plt.show()
+    # model = LinearRegression()
+    # X = data_frame[["Date", "GHI(Wh/m2)"]]
+    # y = data_frame["GHI(t+1)"]
+    # plt.scatter(X, y)
+    # plt.show()
 
 
 def cluster():
